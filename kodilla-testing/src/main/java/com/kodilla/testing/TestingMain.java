973783222b36c2184f6e55dbb4c91bce78c529c4
Jakub Piejko <https://github.com/Kubeus14/Jakub-Piejko-kodilla-java.git>
2020-11-20 16:23:34 +0100

    6.2_poprawka

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -7,7 +7,18 @@ public class TestingMain {
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
 
         Calculator calculator = new Calculator();
-
+        int result = calculator.add(7, 5);
+        if (result == 12) {
+            System.out.println("add is ok");
+        } else {
+            System.out.println("add is wrong");
+        }
+        int result2 = calculator.substract(7, 5);
+        if (result2 == 2) {
+            System.out.println("substract is ok");
+        } else {
+            System.out.println("substract is wrong");
+        }
 
 
     }
