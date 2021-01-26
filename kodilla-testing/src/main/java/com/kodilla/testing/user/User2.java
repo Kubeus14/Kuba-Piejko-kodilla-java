9618a27471621152d2b7e0efa98128981fbe3575
Jakub Piejko <https://github.com/Kubeus14/Jakub-Piejko-kodilla-java.git>
2021-01-26 15:50:42 +0100

    kodilla-course

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/User2.java
@@ -0,0 +1,10 @@
+package com.kodilla.testing.user;
+
+public class User2{
+    public static void main(String[] args){
+        int a=5;
+        int b=3;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
