5cdb9f9f619e4a9b5c0ccfd0a5505ddf880010a1
Jakub Piejko <https://github.com/Kubeus14/Jakub-Piejko-kodilla-java.git>
2021-03-11 17:21:52 +0100

    kodillacourse

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,45 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.task.dao.TaskDao;
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest
+public class TaskListDaoTestSuite {
+    @Autowired
+    private TaskListDao taskListDao;
+    @Autowired
+    private TaskDao taskDao;
+
+    private static final String LISTNAME = "List name";
+    private static final String DESCRIPTION = "Description to execute";
+
+    @Test
+    public void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList(LISTNAME, DESCRIPTION);
+
+
+        //When
+        taskListDao.save(taskList);
+
+
+        //Then
+        List<TaskList> readTaskList = taskListDao.findByListName(LISTNAME);
+
+
+        assertEquals(LISTNAME, readTaskList.get(0).getListName());
+
+
+        //CleanUp
+        taskListDao.delete(taskList);
+
+    }
+}
